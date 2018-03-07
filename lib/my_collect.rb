ea50f6d4b5ae3bbb7147f018df02be651c7e5818
Andrew Kairys <andrewkairys@gmail.com>
2018-03-07 17:31:48 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(array)
   i = 0
   collect = []
   while array.length > i do |i|
-    collect << yield (array[i])
+    yield (array[i])
   end
     i += 1
   end
-  array
+  collect
 end
