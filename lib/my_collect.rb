5ef4f615c1f2219f7e0add7688e1daad14eabd78
Andrew Kairys <andrewkairys@gmail.com>
2018-03-07 17:40:19 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   collect = []
   while array.length > i
-    collect << yield array[i]
+    collect << array[i]
     i += 1
   end
   collect {|i| i.first.capitalize}
